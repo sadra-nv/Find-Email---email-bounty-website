c198bd49c94d9a1fda94fd31cdb30d72acd557f0
spdfalcon <spd.mohammadreza@gmail.com>
2024-09-21 06:23:54 -0700

    footer

--- app/components/modules/Footer/Footer.tsx
+++ app/components/modules/Footer/Footer.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='bg-gradient-to-b from-[#8F5CE1] to-[#2E2291]'>
+    <div className="bg-[url('/images/bgfooter.svg')] h-[268px]">
+        mohammad
+    </div>
+    </div>
   )
 }
