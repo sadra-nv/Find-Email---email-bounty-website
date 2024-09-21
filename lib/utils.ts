5343628260944b7ef52aae9a1edf96ae4b228618
[rezaeiar] <rezaeiar0@gmail.com>
2024-09-21 13:31:48 +0330

    fix main section of blog

---
+++ lib/utils.ts
@@ -0,0 +1,13 @@
+export function generateDots(count: number, ctx: CanvasRenderingContext2D) {
+    for (let i = 0; i < count; i++) {
+      const x = Math.random() * ctx.canvas.width;
+      const y = Math.random() * ctx.canvas.height;
+      const radius = 1;
+  
+      ctx.beginPath();
+      ctx.arc(x, y, radius, 0, Math.PI * 2);
+      ctx.fillStyle = "#9c3f74";
+      ctx.fill();
+    }
+  }
+  
