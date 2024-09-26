0e0614061672696dfd32bdd14b5cc55dc86f0668
spdfalcon <spd.mohammadreza@gmail.com>
2024-09-26 03:26:35 -0700

    responsive footer

--- app/components/modules/Footer/Footer.tsx
+++ app/components/modules/Footer/Footer.tsx
@@ -3,17 +3,19 @@ import React from "react";
 export default function Footer() {
   return (
     <>
-      <div className="bg-gradient-to-b from-[#8F5CE1] to-[#2E2291]">
-        <div className="bg-[url('/images/bgfooter.svg')] p-8 md:container flex flex-col-reverse md:flex-row gap-10 md:justify-between">
-          <div className="b l flex justify-between md:flex-row-reverse w-full">
-            <ul className="flex flex-col gap-5  text-xs font-normal">
-              <li className="text-base font-bold">site</li>
+    <div className="bg-gradient-to-b from-[#8F5CE1] to-[#2E2291]">
+      <div className="bg-[url('/images/bgfooter.svg')]">
+      <div className="container">
+        <div className=" p-8  flex flex-col-reverse md:flex-row gap-10 md:justify-between">
+          <div className="b l flex justify-evenly md:flex-row-reverse w-full">
+            <ul className="flex flex-col gap-5  text-xs font-normal md:text-base">
+              <li className="text-base md:text-[22px] font-bold">site</li>
               <li className="opacity-75">Pricing</li>
               <li className="opacity-75">Blog</li>
               <li className="opacity-75">Support</li>
             </ul>
-            <ul className="flex flex-col gap-5  text-xs font-normal">
-              <li className="text-base font-bold">Pages</li>
+            <ul className="flex flex-col gap-5  text-xs font-normal md:text-base">
+              <li className="text-base md:text-[22px] font-bold">Pages</li>
               <li className="opacity-75">Pricing</li>
               <li className="opacity-75">Forum</li>
               <li className="opacity-75">Contact</li>
@@ -22,24 +24,28 @@ export default function Footer() {
             <div className="flex items-center">
               <ul className="flex flex-col items-center justify-center  gap-5 md:gap-3  text-xs font-normal md:grid md:grid-cols-2 h-fit">
                 <li className="">
-                  <img src="/icons/Twitter1.svg" alt="" />
+                  <img className="md:hidden" src="/icons/Twitter1.svg" alt="" />
+                  <img className="hidden md:block" src="/icons/Twitter1md.svg" alt="" />
                 </li>
                 <li className="">
-                  <img src="/icons/Instagram1.svg" alt="" />
+                  <img className="md:hidden" src="/icons/Instagram1.svg" alt="" />
+                  <img className="hidden md:block" src="/icons/Instagrammd1.svg" alt="" />
                 </li>
                 <li className="">
-                  <img src="/icons/Facebook2.svg" alt="" />
+                  <img className="md:hidden" src="/icons/Facebook2.svg" alt="" />
+                  <img className="hidden md:block" src="/icons/Facebookmd2.svg" alt="" />
                 </li>
                 <li className="">
-                  <img src="/icons/LinkedIn.svg" alt="" />
+                  <img className="md:hidden" src="/icons/LinkedIn.svg" alt="" />
+                  <img className="hidden md:block" src="/icons/LinkedInmd.svg" alt="" />
                 </li>
               </ul>
             </div>
           </div>
 
           <div className="t r flex flex-col gap-5">
-            <h2 className="font-bold">Find email addresses in seconds</h2>
-            <p className="text-xs opacity-75">Subscribe to newsletter</p>
+            <h2 className="font-bold md:text-[22px]">Find email addresses in seconds</h2>
+            <p className="text-xs opacity-75 md:text-base">Subscribe to newsletter</p>
             <div className="flex w-[248px] h-10 rounded-md overflow-hidden">
               <input
                 placeholder="Your email"
@@ -55,6 +61,8 @@ export default function Footer() {
           </div>
         </div>
       </div>
+      </div>
+    </div>
       <div className="flex justify-center text-xs py-5">
         <p>©2024 Find Email All rights reserved.</p>
       </div>
