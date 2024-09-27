cb5a3a4954464b723603d7db5d6825ab1273691e
spdfalcon <spd.mohammadreza@gmail.com>
2024-09-27 02:08:15 -0700

    end responsive blogs

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,17 +1,15 @@
-import CategoryList from "../components/templates/home/CategoryList/CategoryList"
-import HeroSection from "../components/templates/home/HeroSection/HeroSection"
-import FilterableCardGallery from "../components/templates/home/FilterableCardGallery/FilterableCardGallery"
-import SiteOverview from "../components/templates/home/SiteOverview/SiteOverview"
+import CategoryList from "../components/templates/home/CategoryList/CategoryList";
+import HeroSection from "../components/templates/home/HeroSection/HeroSection";
+import FilterableCardGallery from "../components/templates/home/FilterableCardGallery/FilterableCardGallery";
+import SiteOverview from "../components/templates/home/SiteOverview/SiteOverview";
 
 export default function page() {
-    return (
-        <>
-            <HeroSection></HeroSection>
-            <div className="">
-                <CategoryList></CategoryList>
-                {/* <FilterableCardGallery></FilterableCardGallery> */}
-            </div>
-            {/* <SiteOverview></SiteOverview> */}
-        </>
-    )
+  return (
+    <>
+      <HeroSection></HeroSection>
+      <CategoryList></CategoryList>
+      <FilterableCardGallery></FilterableCardGallery>
+      {/* <SiteOverview></SiteOverview> */}
+    </>
+  );
 }
