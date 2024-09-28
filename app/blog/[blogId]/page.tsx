8ef2bd996c592888a6c6945205ee1df90d5cfd9b
spdfalcon <spd.mohammadreza@gmail.com>
2024-09-27 22:40:44 -0700

    end responsive single blog

--- app/blog/[blogId]/page.tsx
+++ app/blog/[blogId]/page.tsx
@@ -1,143 +1,181 @@
-import Link from 'next/link'
-import React from 'react'
+import HeroSection from "@/app/components/templates/home/HeroSection/HeroSection";
+import Link from "next/link";
+import React from "react";
 
 export default function page() {
-    return (
-        <div>
-            <div className="">
-                <div className="container">
-                    <div className="pt-52">
-                        <div className="mb-8">
-                            <h1 className='text-4xl font-bold mb-5'>
-                                Multi Thread SMTP Cracker
-                            </h1>
-                            <p className='text-lg'>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Egestas purus viverra accumsan in nisl nisi Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque
-                            </p>
-                        </div>
-                        <div className="grid grid-cols-7 gap-6 pb-8">
-                            <div className="col-span-5">
-                                <div className="w- mb-6">
-                                    <img src="/images/445.png" className='w-full' alt="" />
-                                </div>
-                                <div className="">
-                                    <button>
-                                        <Link href={'/'} className='flex items-center gap-1 p-5 rounded-lg text-xs text-black bg-gradient-to-t from-[#B1A110] to-[#FFF280]'>
-                                            New Database
-                                        </Link>
-                                    </button>
-                                    <p className='text-base/8 mt-8'>
-                                        We have updated <br />
-                                        We have updated our database, and it now contains a large number of records and valuable information. <br />
-                                        Currently, our database includes over <br />
-                                    </p>
-                                    <p className="text-base/8 mt-8">
-                                        Company Email Record : <span className='text-[#4DE649]'>449,295,742</span><br />
-                                        Leak database Record : <span className='text-[#4DE649]'>6,022,484,191</span><br />
-                                        Logs [ url:login:pass ] Record : <span className='text-[#4DE649]'>1,786,664,764</span>
-                                    </p>
-                                    <p className="text-base/8 mt-8">
-                                        and this number is increasing daily.
-                                    </p>
-                                    <p className="text-base/8 mt-8">
-                                        This database is constantly being updated every day, every moment. <br />
-                                        We strive to keep our database up-to-date and comprehensive every day so that we can provide you with the best and most accurate information. Our goal is to help you access the data you need with the help of this continuously improving database.
-                                    </p>
-                                </div>
-                                <div className="flex items-center gap-4 mt-6">
-                                    <button>
-                                        <Link href={'/'} className='flex items-center gap-1 p-5 rounded-lg text-xs text-black bg-gradient-to-t from-[#B1A110] to-[#FFF280]'>
-                                            New Database
-                                        </Link>
-                                    </button>
-                                    <button>
-                                        <Link href={'/'} className='flex items-center gap-1 p-5 rounded-lg text-xs text-white bg-gradient-to-t from-[#2E2291] to-[#8F5CE1]'>
-                                            database
-                                        </Link>
-                                    </button>
-                                    <button>
-                                        <Link href={'/'} className='flex items-center gap-1 p-5 rounded-lg text-xs text-white bg-gradient-to-t from-[#2E2291] to-[#8F5CE1]'>
-                                            database
-                                        </Link>
-                                    </button>
-                                    <button>
-                                        <Link href={'/'} className='flex items-center gap-1 p-5 rounded-lg text-xs text-white bg-gradient-to-t from-[#2E2291] to-[#8F5CE1]'>
-                                            database
-                                        </Link>
-                                    </button>
-                                    <button>
-                                        <Link href={'/'} className='flex items-center gap-1 p-5 rounded-lg text-xs text-white bg-gradient-to-t from-[#2E2291] to-[#8F5CE1]'>
-                                            database
-                                        </Link>
-                                    </button>
-                                </div>
-                            </div>
-                            <div className="col-span-2 flex flex-col gap-6">
-                                <div className="flex flex-col rounded-lg overflow-hidden bg-[#0B0B25]">
-                                    <div className="text-sm py-4 bg-gradient-to-t from-[#07255B] to-[#061742] text-center">
-                                        Categories
-                                    </div>
-                                    <ul className="flex flex-col gap-4 p-4">
-                                        <li>
-                                            <Link href={'/'} className="flex justify-between items-center p-4 bg-[#131332] rounded-lg">
-                                                <span className="text-xs">
-                                                    url : login : pass
-                                                </span>
-                                                <span className="text-[10px]">
-                                                    (3)
-                                                </span>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href={'/'} className="flex justify-between items-center p-4 bg-[#131332] rounded-lg">
-                                                <span className="text-xs">
-                                                    url : login : pass
-                                                </span>
-                                                <span className="text-[10px]">
-                                                    (3)
-                                                </span>
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                                <div className="flex flex-col rounded-lg overflow-hidden bg-[#0B0B25]">
-                                    <div className="text-sm py-4 bg-gradient-to-t from-[#07255B] to-[#061742] text-center">
-                                        Post
-                                    </div>
-                                    <ul className="flex flex-col gap-4 p-4">
-                                        <li>
-                                            <Link href={'/'} className="flex gap-3 items-center p-4 bg-[#131332] rounded-lg">
-                                                <img src="/images/546 4.png" alt="" />
-                                                <div className="flex flex-col text-xs gap-1">
-                                                    <p>
-                                                        FindEmail.io:
-                                                    </p>
-                                                    <span>
-                                                        Secret Weapon for
-                                                    </span>
-                                                </div>
-                                            </Link>
-                                        </li>
-                                        <li>
-                                            <Link href={'/'} className="flex gap-3 items-center p-4 bg-[#131332] rounded-lg">
-                                                <img src="/images/546 4.png" alt="" />
-                                                <div className="flex flex-col text-xs gap-1">
-                                                    <p>
-                                                        FindEmail.io:
-                                                    </p>
-                                                    <span>
-                                                        Secret Weapon for
-                                                    </span>
-                                                </div>
-                                            </Link>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div>
+      <div className="">
+        <div className="container">
+          <div className="md:pt-52 pt-32">
+            <div className="mb-8">
+              <h1 className="text-xl xl:text-4xl font-bold mb-5">
+                Multi Thread SMTP Cracker
+              </h1>
+              <p className="text-sm/8 xl:text-lg">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua
+                Egestas purus viverra accumsan in nisl nisi Arcu cursus vitae
+                congue mauris rhoncus aenean vel elit scelerisque
+              </p>
             </div>
+            <div className="grid xl:grid-cols-7 gap-6 pb-8">
+              <div className="xl:col-span-5">
+                <div className="w- mb-6">
+                  <img src="/images/445.png" className="w-full" alt="" />
+                </div>
+                <div className="">
+                  <button>
+                    <Link
+                      href={"/"}
+                      className="flex items-center gap-1 px-[18px] py-3 rounded-lg text-xs text-black bg-gradient-to-t from-[#B1A110] to-[#FFF280]"
+                    >
+                      New Database
+                    </Link>
+                  </button>
+                  <p className="text-sm/8 xl:text-base/8 mt-8">
+                    We have updated <br />
+                    We have updated our database, and it now contains a large
+                    number of records and valuable information. <br />
+                    Currently, our database includes over <br />
+                  </p>
+                  <p className="text-sm/8 xl:text-base/8 mt-8">
+                    Company Email Record :{" "}
+                    <span className="text-[#4DE649]">449,295,742</span>
+                    <br />
+                    Leak database Record :{" "}
+                    <span className="text-[#4DE649]">6,022,484,191</span>
+                    <br />
+                    Logs [ url:login:pass ] Record :{" "}
+                    <span className="text-[#4DE649]">1,786,664,764</span>
+                  </p>
+                  <p className="text-sm/8 xl:text-base/8 mt-8">
+                    and this number is increasing daily.
+                  </p>
+                  <p className="text-sm/8 xl:text-base/8 mt-8">
+                    This database is constantly being updated every day, every
+                    moment. <br />
+                    We strive to keep our database up-to-date and comprehensive
+                    every day so that we can provide you with the best and most
+                    accurate information. Our goal is to help you access the
+                    data you need with the help of this continuously improving
+                    database.
+                  </p>
+                </div>
+                <div className="flex flex-wrap items-center gap-4 mt-6">
+                  <button>
+                    <Link
+                      href={"/"}
+                      className="flex items-center gap-1 px-[18px] py-3 rounded-lg text-xs text-black bg-gradient-to-t from-[#B1A110] to-[#FFF280]"
+                    >
+                      New Database
+                    </Link>
+                  </button>
+                  <button>
+                    <Link
+                      href={"/"}
+                      className="flex items-center gap-1 px-[18px] py-3 rounded-lg text-xs text-white bg-gradient-to-t from-[#2E2291] to-[#8F5CE1]"
+                    >
+                      database
+                    </Link>
+                  </button>
+                  <button>
+                    <Link
+                      href={"/"}
+                      className="flex items-center gap-1 px-[18px] py-3 rounded-lg text-xs text-white bg-gradient-to-t from-[#2E2291] to-[#8F5CE1]"
+                    >
+                      database
+                    </Link>
+                  </button>
+                  <button>
+                    <Link
+                      href={"/"}
+                      className="flex items-center gap-1 px-[18px] py-3 rounded-lg text-xs text-white bg-gradient-to-t from-[#2E2291] to-[#8F5CE1]"
+                    >
+                      database
+                    </Link>
+                  </button>
+                  <button>
+                    <Link
+                      href={"/"}
+                      className="flex items-center gap-1 px-[18px] py-3 rounded-lg text-xs text-white bg-gradient-to-t from-[#2E2291] to-[#8F5CE1]"
+                    >
+                      database
+                    </Link>
+                  </button>
+                </div>
+              </div>
+              <div className="xl:col-span-2 flex flex-col gap-6">
+                <div className="flex flex-col rounded-lg overflow-hidden bg-[#0B0B25]">
+                  <div className="text-sm py-4 bg-gradient-to-t from-[#07255B] to-[#061742] text-center">
+                    Categories
+                  </div>
+                  <ul className="flex flex-col gap-4 p-4">
+                    <li>
+                      <Link
+                        href={"/"}
+                        className="flex justify-between items-center p-4 bg-[#131332] rounded-lg"
+                      >
+                        <span className="text-xs">url : login : pass</span>
+                        <span className="text-[10px]">(3)</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        href={"/"}
+                        className="flex justify-between items-center p-4 bg-[#131332] rounded-lg"
+                      >
+                        <span className="text-xs">url : login : pass</span>
+                        <span className="text-[10px]">(3)</span>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+                <div className="flex flex-col rounded-lg overflow-hidden bg-[#0B0B25]">
+                  <div className="text-sm py-4 bg-gradient-to-t from-[#07255B] to-[#061742] text-center">
+                    Post
+                  </div>
+                  <ul className="flex flex-col gap-4 p-4">
+                    <li>
+                      <Link
+                        href={"/"}
+                        className="flex gap-3 items-center p-4 bg-[#131332] rounded-lg"
+                      >
+                        <img src="/images/546 4.png" alt="" />
+                        <div className="flex flex-col text-xs gap-1">
+                          <p>FindEmail.io:</p>
+                          <span>Secret Weapon for</span>
+                        </div>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        href={"/"}
+                        className="flex gap-3 items-center p-4 bg-[#131332] rounded-lg"
+                      >
+                        <img src="/images/546 4.png" alt="" />
+                        <div className="flex flex-col text-xs gap-1">
+                          <p>FindEmail.io:</p>
+                          <span>Secret Weapon for</span>
+                        </div>
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+        <div className="relative">
+          <video className="mix-blend-lighten opacity-15 " autoPlay muted loop>
+            <source src="/videos/q-c3d7becf.webm" type="video/webm" />
+            Your browser does not support the video tag.
+          </video>
+          <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 text-white text-xs md:text-xl xl:text-3xl">
+            Related Posts
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
