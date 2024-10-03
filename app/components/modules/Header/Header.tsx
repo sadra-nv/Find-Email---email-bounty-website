0f25893393c3a8d3a480e33af13a3225ccde33a0
[rezaeiar] <rezaeiar0@gmail.com>
2024-10-03 12:28:58 +0330

    fix hover effect

--- app/components/modules/Header/Header.tsx
+++ app/components/modules/Header/Header.tsx
@@ -2,66 +2,70 @@ import Link from "next/link";
 import React from "react";
 
 export default function Header() {
-  return (
-    <header className="absolute top-0 left-0 w-full z-50">
-      <div className="container">
-        <div className="flex justify-between items-center py-12">
-          <Link href={"/"} className="flex items-center gap-2.5">
-            <img className="" src="/images/logomd.svg" alt="" />
+    return (
+        <header className="absolute top-0 left-0 w-full z-50">
+            <div className="container">
+                <div className="flex justify-between items-center py-12">
+                    <Link href={"/"} className="flex items-center gap-2.5">
+                        <img className="" src="/images/logomd.svg" alt="" />
 
-            <h4 className="text-xl text-white font-bold">FindEmail</h4>
-          </Link>
-          <nav>
-            <ul className="flex items-center gap-6 bg-white/15 rounded-xl text-white text-sm px-6 py-5">
-              <li>
-                <Link href={"/"} className="flex items-center gap-1">
-                  <img src="/icons/Group.svg" alt="" />
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link href={"/blog"} className="flex items-center gap-1">
-                  <img src="/icons/8 1.svg" alt="" />
-                  Blog
-                </Link>
-              </li>
-              <li>
-                <Link href={"/"} className="flex items-center gap-1">
-                  <img src="/icons/Group 238340.svg" alt="" />
-                  Pricing
-                </Link>
-              </li>
-              <li>
-                <Link href={"/"} className="flex items-center gap-1">
-                  <img src="/icons/9079356 1.svg" alt="" />
-                  Online Tools
-                </Link>
-              </li>
-              <li>
-                <Link href={"/"} className="flex items-center gap-1">
-                  <img src="/icons/11 1.svg" alt="" />
-                  FAQ
-                </Link>
-              </li>
-              <li>
-                <Link href={"/"} className="flex items-center gap-1">
-                  <img src="/icons/Customer Support 1.svg" alt="" />
-                  Contact us
-                </Link>
-              </li>
-            </ul>
-          </nav>
-          <button>
+                        <h4 className="text-xl text-white font-bold">FindEmail</h4>
+                    </Link>
+                    <nav>
+                        <ul className="flex items-center gap-6 bg-white/15 rounded-xl text-white text-sm px-6 py-5">
+                            <li>
+                                <Link href={"/"} className="flex items-center gap-1">
+                                    <img src="/icons/Group.svg" alt="" />
+                                    Home
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={"/blog"} className="flex items-center gap-1">
+                                    <img src="/icons/8 1.svg" alt="" />
+                                    Blog
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={"/"} className="flex items-center gap-1">
+                                    <img src="/icons/Group 238340.svg" alt="" />
+                                    Pricing
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={"/"} className="flex items-center gap-1">
+                                    <img src="/icons/9079356 1.svg" alt="" />
+                                    Online Tools
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={"/"} className="flex items-center gap-1">
+                                    <img src="/icons/11 1.svg" alt="" />
+                                    FAQ
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href={"/"} className="flex items-center gap-1">
+                                    <img src="/icons/Customer Support 1.svg" alt="" />
+                                    Contact us
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <button className="before:ease relative overflow-hidden bg-gradient-to-r border border-[#c50c1883] from-[#c50c1969] to-[#a50b156e] text-white shadow-2xl transition-all before:absolute before:right-0 before:top-0 before:h-full before:w-6 before:translate-x-12 before:rotate-6 before:bg-white before:opacity-10 before:duration-700 hover:before:-translate-x-40 rounded-lg flex items-center justify-center text-xs px-5 py-4 gap-1">
+                        Login/Register
+                        <img src="/icons/13 1.svg" alt="" />
+                    </button>
+                    {/* <button>
             <Link
               href={"/"}
-              className="flex items-center gap-1 p-5 rounded-lg text-xs text-white bg-gradient-to-t from-[#2E2291] to-[#8F5CE1]"
+              className="flex items-center gap-1 px-5 py-4 rounded-lg text-xs text-white bg-gradient-to-r from-[#C24B76] to-[#FD8330] hover:opacity-75"
             >
               Login/Register
               <img src="/icons/13 1.svg" alt="" />
             </Link>
-          </button>
-        </div>
-      </div>
-    </header>
-  );
+          </button> */}
+                </div>
+            </div>
+        </header>
+    );
 }
