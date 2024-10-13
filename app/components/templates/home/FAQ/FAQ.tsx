0ff2d2fc851cfd8523667e6bf5dc4b2268ba6ba5
[rezaeiar] <rezaeiar0@gmail.com>
2024-10-13 15:23:05 +0330

    fix FAQ

---
+++ app/components/templates/home/FAQ/FAQ.tsx
@@ -0,0 +1,105 @@
+import React from 'react'
+
+export default function FAQ() {
+    return (
+        <div>
+            <div className="container">
+                <div className="">
+                    <div className="flex justify-center">
+                        <img src="./images/home/q-97c536f9.png (1) 2.png" alt="" />
+                    </div>
+                    <div className="flex gap-2 items-center mb-8">
+                        <span className='grow bg-gradient-to-l from-white via-transparent to-transparent h-0.5'></span>
+                        <h5 className='font-bold text-3xl'>
+                            FAQ
+                        </h5>
+                        <span className='grow bg-gradient-to-r from-white via-transparent to-transparent h-0.5'></span>
+                    </div>
+                    <div className="*:text-center mb-12">
+                        <h4 className='text-2xl font-bold mb-5'>
+                            Frequently asked questions
+                        </h4>
+                        <p className='text-lg'>
+                            Browse through these FAQs to find answers to commonly asked questions.
+                        </p>
+                    </div>
+                    <div className="grid grid-cols-2">
+                        <div className="flex justify-center items-center">
+                            <img src="./images/home/m011t0438_g_interrogation_box_07sep22 1.png" alt="" />
+                        </div>
+                        <div className="flex flex-col gap-5 relative">
+                            <img src="./vectors/Vector.svg" className='w-full absolute' alt="" />
+                            <div className="bg-white/10 rounded-xl p-5 flex justify-between items-center">
+                                <p className='text-sm font-bold'>
+                                    how many email included in our service?
+                                </p>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
+                                    <path fillRule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+                                </svg>
+                            </div>
+                            <div className="bg-white/10 rounded-xl p-5 flex justify-between items-center">
+                                <p className='text-sm font-bold'>
+                                    how many email included in our service?
+                                </p>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
+                                    <path fillRule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+                                </svg>
+                            </div>
+                            <div className="bg-white/10 rounded-xl p-5 flex justify-between items-center">
+                                <p className='text-sm font-bold'>
+                                    how many email included in our service?
+                                </p>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
+                                    <path fillRule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+                                </svg>
+                            </div>
+                            <div className="bg-white/10 rounded-xl p-5 flex justify-between items-center">
+                                <p className='text-sm font-bold'>
+                                    how many email included in our service?
+                                </p>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
+                                    <path fillRule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+                                </svg>
+                            </div>
+                            <div className="bg-white/10 rounded-xl p-5 flex justify-between items-center">
+                                <p className='text-sm font-bold'>
+                                    how many email included in our service?
+                                </p>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
+                                    <path fillRule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+                                </svg>
+                            </div>
+                            <div className="bg-white/10 rounded-xl p-5 flex justify-between items-center">
+                                <p className='text-sm font-bold'>
+                                    how many email included in our service?
+                                </p>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
+                                    <path fillRule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+                                </svg>
+                            </div>
+                            <div className="bg-white/10 rounded-xl p-5 flex justify-between items-center">
+                                <p className='text-sm font-bold'>
+                                    how many email included in our service?
+                                </p>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
+                                    <path fillRule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex justify-center items-center">
+                        <video
+                            className="mix-blend-lighten relative z-10"
+                            autoPlay
+                            muted
+                            loop
+                        >
+                            <source src="/videos/q-c3d7becff.webm" type="video/webm" />
+                            Your browser does not support the video playback.
+                        </video>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import FAQ from "./components/templates/home/FAQ/FAQ";
 import SelectHome from "./components/templates/home/SelectHome/SelectHome";
 
 export default function Home() {
   return (
-    <div className="container mt-40 relative z-30">
+    <div className="mt-40 relative z-30">
         <SelectHome></SelectHome>
+        <FAQ></FAQ>
     </div>
   );
 }
