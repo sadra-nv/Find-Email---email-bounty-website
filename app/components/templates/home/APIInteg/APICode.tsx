955fc551d995b0957555b940133a1d1327326d75
SadraNV <sadra1382dg@gmail.com>
2024-10-20 20:57:21 +0330

    fix(home-page): - 500  error fix

--- app/components/templates/home/APIInteg/APICode.tsx
+++ app/components/templates/home/APIInteg/APICode.tsx
@@ -10,11 +10,7 @@ export default function APICode() {
         to seamlessly integrate all other functions and the database in your
         organization’s infrastructure
       </p>
-      <div
-        className="bg-[#0C172D] h-[31.25rem] rounded-lg shadow-api-code relative before:-top-6
-       before:left-1/2 before:-translate-x-0 before:bg-highlight-light before:w-12 before:h-12 before:absolute
-    blur-2xl z-10 before:z-0"
-      ></div>
+      <div className="bg-[#0C172D] h-[31.25rem] rounded-lg shadow-api-code "></div>
     </div>
   );
 }
