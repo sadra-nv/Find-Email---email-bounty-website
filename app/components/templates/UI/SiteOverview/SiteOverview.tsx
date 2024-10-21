516498723c2976dfe2dafa93c3f3f2c83c351cef
SadraNV <sadra1382dg@gmail.com>
2024-10-21 15:40:41 +0330

    refactore(home-page): - changed SiteOverview to use Nextjs Image component

--- app/components/templates/UI/SiteOverview/SiteOverview.tsx
+++ app/components/templates/UI/SiteOverview/SiteOverview.tsx
@@ -1,5 +1,13 @@
 import { cn } from "@/lib/utils";
 import React, { ReactNode } from "react";
+import ball from "@/public/images/particle-3d-ball.png";
+import bg from "@/public/vectors/Background 1.png";
+import email from "@/public/images/Email 1.png";
+import chart from "@/public/images/grows.png";
+import list from "@/public/images/list.png";
+import data from "@/public/images/data_management_10 1.png";
+import Image from "next/image";
+import CodeVideo from "../CodeVideo/CodeVideo";
 
 export default function SiteOverview({
   children,
@@ -29,79 +37,85 @@ export default function SiteOverview({
       <div className="container">
         <div className="flex flex-col pb-10">
           <div className="flex justify-center relative items-center">
-            <img
-              src="/vectors/Background 1.png"
+            <Image
+              alt="background"
+              src={bg}
               className="absolute h-full object-cover"
-              alt=""
+              placeholder="blur"
             />
+
             <div className="block absolute inset-0 mx-auto h-auto sm:h-full aspect-square pointer-events-none research-radar-scanner-wrapper-visible m-auto animate-normal">
               <div className="absolute h-full w-full -rotate-[201deg] research-radar-scanner">
-                <div className="h-full w-full rounded-full before:content-[''] before:absolute before:w-full before:h-full before:bg-[linear-gradient(267deg,rgba(99,102,241,.4)13.23%,rgba(3,0,20,0)50%)] before:rounded-full before:block"></div>
+                <div className="h-full w-full rounded-full  before:absolute before:w-full before:h-full before:bg-[linear-gradient(267deg,rgba(99,102,241,.4)13.23%,rgba(3,0,20,0)50%)] before:rounded-full before:block"></div>
               </div>
             </div>
-            <img
-              src="/icons/Group 238477.svg"
-              className="absolute h-20 sm:h-24 animate-pulse"
-              alt=""
+            <Image
+              alt="particle-ball"
+              src={ball}
+              placeholder="blur"
+              width={97}
+              height={97}
+              className="absolute animate-pulse mix-blend-color-dodge"
             />
-            <video
-              className="hidden lg:block absolute -right-1/2 xl:-right-1/3 h-4/5 mix-blend-lighten opacity-50"
-              autoPlay
-              muted
-              loop
-            >
-              <source src="/videos/q-ba39153a.webm" type="video/webm" />
-              Your browser does not support the video tag.
-            </video>
-            <video
-              className="hidden lg:block absolute -left-1/2 xl:-left-1/3 h-4/5 mix-blend-lighten opacity-50"
-              autoPlay
-              muted
-              loop
-            >
-              <source src="/videos/q-ba39153a.webm" type="video/webm" />
-              Your browser does not support the video tag.
-            </video>
+            <CodeVideo className="-right-1/2 xl:-right-1/3 mix-blend-lighten" />
+            <CodeVideo className="-left-1/2 xl:-left-1/3 mix-blend-lighten" />
+
             <div className="grid grid-cols-2 w-full items-center justify-center sm:px-32">
               <div className="flex flex-col justify-center items-center py-12">
-                <img
-                  src="/vectors/11281907 1.svg"
-                  alt=""
+                <Image
+                  alt="list"
+                  src={list}
+                  placeholder="blur"
+                  width={97}
+                  height={97}
                   className="w-10 sm:w-auto"
                 />
+
                 <span className="text-sm md:text-xl mt-2">2,028,741,188</span>
                 <span className="text-xs md:text-sm mt-1 sm:mt-2">
                   Logs [ url:login:pass ] Record
                 </span>
               </div>
               <div className="flex flex-col justify-center items-center py-12">
-                <img
-                  src="/vectors/m003t0635_i_business_growth_06oct_22 1.svg"
-                  alt=""
+                <Image
+                  alt="grows"
+                  src={chart}
+                  placeholder="blur"
+                  width={97}
+                  height={97}
                   className="w-10 sm:w-auto"
                 />
+
                 <span className="text-sm md:text-xl mt-2">76,523</span>
                 <span className="text-xs md:text-sm mt-1 sm:mt-2">
                   Total Search Record
                 </span>
               </div>
               <div className="flex flex-col justify-center items-center py-12">
-                <img
-                  src="/vectors/Email 2.svg"
-                  alt=""
+                <Image
+                  alt="email"
+                  src={email}
+                  placeholder="blur"
+                  width={97}
+                  height={97}
                   className="w-10 sm:w-auto"
                 />
+
                 <span className="text-sm md:text-xl mt-2">496,580,230</span>
                 <span className="text-xs md:text-sm mt-1 sm:mt-2">
                   Company Email Record
                 </span>
               </div>
               <div className="flex flex-col justify-center items-center py-12">
-                <img
-                  src="/vectors/data_management_10 1.svg"
-                  alt=""
+                <Image
+                  alt="database"
+                  src={data}
+                  placeholder="blur"
+                  width={97}
+                  height={97}
                   className="w-10 sm:w-auto"
                 />
+
                 <span className="text-sm md:text-xl mt-2">6,117,822,935</span>
                 <span className="text-xs md:text-sm mt-1 sm:mt-2">
                   Leak database Record
