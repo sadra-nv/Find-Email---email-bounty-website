55686c2ac943248c45be7fdbcc141bb829401742
SadraNV <sadra1382dg@gmail.com>
2024-10-24 12:10:35 +0330

    refactor(home-page): - added typeing animation to APICode when it gets inside viewport

--- app/components/templates/home/APIInteg/APICode.tsx
+++ app/components/templates/home/APIInteg/APICode.tsx
@@ -1,8 +1,12 @@
-import { Code } from "bright";
+"use client";
+
+import { useObserver } from "@/hooks/useObserver";
+import React, { LegacyRef } from "react";
 
 export default function APICode() {
-  Code.theme = "github-dark";
-  const myCode = `root@criminalip ~ % curl --request GET --url "https://api.criminalip.io/v1/asset/ip/summary?ip=<ip>" --header "x-api-key: <YOUR_API_KEY>" | {
+  const codeIntro = "→ root@criminalip ~ %";
+  const myCode = ` curl --request GET --url "https://api.criminalip.io/v1/asset/ip/summary?ip=<ip>" --header "x-api-key: <YOUR_API_KEY>" |
+   {
    "ip": "8.8.8.8",
    "score": { "inbound": "Moderate", "outbound": "Low" },
    "country": "United States",
@@ -16,9 +20,37 @@ export default function APICode() {
    "latitude": 34.0544,
    "longitude": -118.2441,
    "status": 200
-}
-root@criminalip ~ %
-`;
+}`;
+
+  // const codeRef = useRef<null | HTMLDivElement>(null);
+  // const codeEndRef = useRef<null | HTMLDivElement>(null);
+  // const [index, setIndex] = useState(0);
+  // const typingSpeed = 10;
+
+  const [observeRef, isInView] = useObserver(
+    {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0,
+    },
+    true
+  );
+  // useEffect(() => {
+  //   const typeText = () => {
+  //     if (index < myCode.length && codeRef.current) {
+  //       codeRef.current.textContent += myCode[index];
+  //       setIndex(index + 1);
+  //     }
+  //   };
+
+  //   if (index < myCode.length && isInView) {
+  //     const timeoutId = setTimeout(typeText, typingSpeed);
+  //     return () => clearTimeout(timeoutId);
+  //   }
+  //   if (index >= myCode.length) {
+  //     codeEndRef.current!.style.opacity = "1";
+  //   }
+  // }, [index, myCode, isInView]);
 
   return (
     <div className="row-start-1 row-end-2 lg:-row-start-1 lg:-row-end-2 lg:col-start-2 ">
@@ -31,9 +63,36 @@ root@criminalip ~ %
         to seamlessly integrate all other functions and the database in your
         organization’s infrastructure
       </p>
-      <Code lang="shell" className=" rounded-lg shadow-api-code ">
-        {myCode}
-      </Code>
+
+      <pre
+        ref={observeRef as LegacyRef<HTMLPreElement>}
+        className="bg-back-dark rounded-lg shadow-api-code p-4 lg:p-6 overflow-x-auto thin-bar 
+      font-mono text-wrap whitespace-pre-wrap break-words leading-9 relative overflow-y-hidden"
+      >
+        <span className="text-green-500 ">{codeIntro}</span>
+        <div className="opacity-0">{myCode}</div>
+        <div className="absolute w-full h-full top-8  left-0 p-4 lg:p-6">
+          <code
+            // ref={codeRef}
+            style={{ "--n": myCode.length } as React.CSSProperties}
+            className={`type relative w-full h-fit opacity-0 ${
+              isInView && "code-type"
+            }`}
+          >
+            {myCode}
+          </code>
+        </div>
+        <span
+          // ref={codeEndRef}
+          style={{ "--n": myCode.length } as React.CSSProperties}
+          // style={{ opacity: "0" }}
+          className={`text-green-500 block mt-4 opacity-0 ${
+            isInView && "code-appear"
+          }`}
+        >
+          {codeIntro}
+        </span>
+      </pre>
     </div>
   );
 }
