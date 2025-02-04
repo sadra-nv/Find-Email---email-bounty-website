1da77b37da6184943224959492b36e66d988ada1
SadraNV <sadra1382dg@gmail.com>
2025-02-04 23:34:59 +0330

    feat: blog now sends the ip as headers

--- lib/services/blogs/getBlogSingle.ts
+++ lib/services/blogs/getBlogSingle.ts
@@ -1,4 +1,6 @@
 import { getPlaceholderImage } from "../getPlaceholderImage";
+import crypto from "crypto";
+import { headers } from "next/headers";
 
 interface ArticleSingle {
   ok: boolean;
@@ -24,16 +26,41 @@ interface ArticleSingle {
   };
 }
 
+const publicKey = `-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC/l7eMhPuYvbJwpH7N6tK2SLIE
+9pU+zBLfXDBaHWrWb1xsw26+GkaP5i22SYLsRo2g++5bHGloYOKLDmnrqVZt54fD
+FXrqokN6TuzfRKeVwI0dB+sGhNB20DwM2xcbaphnrP/Qe5OOh27B59SHfEj2axyQ
+yH2VtvfnzptTc9FfcwIDAQAB
+-----END PUBLIC KEY-----`;
+
+function encryptData(data: string): string {
+  const encryptedData = crypto.publicEncrypt(
+    {
+      key: publicKey,
+      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+      oaepHash: "sha256",
+    },
+    // Convert Buffer to Uint8Array
+    new Uint8Array(Buffer.from(data))
+  );
+
+  return encryptedData.toString("base64");
+}
+
+const articleApiUrl = process.env.BLOG_POST_SINGLE_URL as string;
 export async function getBlogSingle(
   slug: string
 ): Promise<ArticleSingle | false> {
-  const articleApiUrl = process.env.BLOG_POST_SINGLE_URL as string;
+  const ip = headers().get("x-forwarded-for") || "127.0.0.1";
+  const encryptedIP = encryptData(ip);
 
-  console.log(slug, "slug");
+  // console.log("ip", encryptedIP);
+
   const body: RequestInit = {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
+      "x-encrypted": encryptedIP,
     },
 
     body: JSON.stringify({ slug: slug }),
@@ -56,12 +83,3 @@ export async function getBlogSingle(
     return false;
   }
 }
-
-// export default async function BlogPage({
-//     params,
-//   }: {
-//     params: { slug: string };
-//   }) {
-//     const article = await getSingleArticle(params.slug);
-//     console.log(article, "5465879848dwa");
-//     if (!article) return;
